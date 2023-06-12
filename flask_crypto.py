c90354a32703be426af1c9ce9863fa7049c7e9ee
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-06-12 15:43:18 +0800

    Add files via upload

--- flask_crypto.py
+++ flask_crypto.py
@@ -25,28 +25,16 @@ def crypto_pre():
     res_data = pd.DataFrame(p)
     res_data['date'] = res_data.iloc[:,1]
     res_data['pingjia'] = res_data.iloc[:,2]
-    res_data['usdt_logo'] = res_data.iloc[:,3]
-    res_data['logo'] = res_data.iloc[:,4]
-    res_data['max_value_time'] = res_data.iloc[:,5]
-    res_data['min_value_time'] = res_data.iloc[:,6]
     res_data['date'] = pd.to_datetime(res_data['date'])
     #res_data['up_date'] = pd.to_datetime(res_data['up_date'])
     res_data = res_data[res_data.date==pd.to_datetime(date)]
 
     if len(res_data) == 0:
         r_pingjia = 'error'
-        usdt_logo = 0
-        logo = 0
-        max_value_time = 0
-        min_value_time = 0
     else:   
         r_pingjia = res_data['pingjia'][0]
-        usdt_logo = res_data['usdt_logo'][0]
-        logo = res_data['logo'][0]
-        max_value_time = res_data['max_value_time'][0]
-        min_value_time = res_data['min_value_time'][0]
 
-    res_dict = {'pingjia':r_pingjia,'usdt_logo':usdt_logo,'logo':logo,'max_value_time':max_value_time,'min_value_time':min_value_time}
+    res_dict = {'pingjia':r_pingjia}
 
     ans_str = json.dumps(res_dict)
     return ans_str
