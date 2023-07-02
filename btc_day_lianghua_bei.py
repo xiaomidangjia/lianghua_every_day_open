527cbca6a69f13870cfe72313ca02054a6bb992a
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-07-02 21:01:00 +0800

    Add files via upload

--- btc_day_lianghua_bei.py
+++ btc_day_lianghua_bei.py
@@ -14,7 +14,6 @@ import datetime
 # 每天早上8点05分运行
 date_now = str(datetime.datetime.utcnow())[0:10]
 # 引入永续合约流动性的概念
-# 引入永续合约流动性的概念
 url_address = ['https://api.glassnode.com/v1/metrics/derivatives/futures_liquidated_volume_long_relative',
               'https://api.glassnode.com/v1/metrics/market/price_usd_close']
 url_name = ['futures','price']
@@ -44,49 +43,96 @@ for i in range(len(data_list)):
 futures_data = result_data[(result_data.date>='2013-01-01')]
 futures_data = futures_data.sort_values(by=['date'])
 futures_data = futures_data.reset_index(drop=True)
-futures_data['future'] = futures_data['value_x']
-futures_data['price'] = futures_data['value_y']
-new_futures_data = futures_data[['date','future','price']]
-# 读取原始数据
-new_futures_data['next_price'] = new_futures_data['price'].shift(-1)
+futures_data['next_value'] = futures_data['value_y'].shift(-1)
 flag_1 = []
-for i in range(len(new_futures_data)):
-    if new_futures_data['next_price'][i] > new_futures_data['price'][i]:
+flag_2 = []
+flag_3 = []
+for i in range(len(futures_data)):
+    if futures_data['next_value'][i] > futures_data['value_y'][i]:
         flag_1.append(1)
     else:
         flag_1.append(0)
-new_futures_data['flag_1'] = flag_1
-
-ins = new_futures_data
-futures_value_1 = ins['future'][len(ins)-1]
-futures_value_2 = ins['future'][len(ins)-2]
-futures_value_3 = ins['future'][len(ins)-3]
-futures_value_4 = ins['future'][len(ins)-4]
-futures_accury_2 = ins['flag_1'][len(ins)-2]
-futures_accury_3 = ins['flag_1'][len(ins)-3]
-futures_accury_4 = ins['flag_1'][len(ins)-4]
-if futures_value_1 > 0.5 and futures_value_2 > 0.5 and futures_value_3 > 0.5 and futures_value_4 > 0.5 and futures_accury_2 + futures_accury_3 + futures_accury_4 ==0:
-    pingjia = 'duotou_continue_kill'
-elif futures_value_1 < 0.5 and futures_value_2 < 0.5 and futures_value_3 < 0.5 and futures_value_4 < 0.5 and futures_accury_2 == 1 and futures_accury_3 ==1 and futures_accury_4 == 1:
-    pingjia = 'kongtou_continue_kill'
-
-elif futures_value_1 > 0.5 and futures_value_2 > 0.5 and futures_value_3 > 0.5 and futures_accury_2 == 0 and futures_accury_3 == 0:
+    if futures_data['value_x'][i] > 0.5:
+        flag_2.append(1)
+    else:
+        flag_2.append(0)
+futures_data['flag_1'] = flag_1
+futures_data['flag_2'] = flag_2
+for i in range(len(futures_data)):
+    if futures_data['flag_1'][i] == futures_data['flag_2'][i]:
+        flag_3.append(1)
+    else:
+        flag_3.append(0)
+futures_data['flag_3'] = flag_3
+futures_value_1 = futures_data['value_x'][len(futures_data)-1]
+futures_value_2 = futures_data['value_x'][len(futures_data)-2]
+futures_value_3 = futures_data['value_x'][len(futures_data)-3]
+futures_accury_2 = futures_data['flag_3'][len(futures_data)-2]
+futures_accury_3 = futures_data['flag_3'][len(futures_data)-3]
+if futures_value_1 > 0.5 and futures_value_2 > 0.8 and futures_value_3 > 0.8 and futures_accury_2 == 0 and futures_accury_3 == 0:
     pingjia = 'duotou_finish_kill'
-elif futures_value_1 > 0.5 and futures_value_2 > 0.5 and futures_accury_2 == 0:
+elif futures_value_1 > 0.8 and futures_value_2 > 0.8 and futures_accury_2 == 0:
     pingjia = 'duotou_ing_kill'
-    
-elif futures_value_1 < 0.5 and futures_value_2 < 0.5 and futures_value_3 < 0.5 and futures_accury_2 == 0 and futures_accury_3 == 0:
+elif futures_value_1 < 0.5 and futures_value_2 < 0.2 and futures_value_3 < 0.2 and futures_accury_2 == 0 and futures_accury_3 == 0:
     pingjia = 'kongtou_finish_kill'
-
-elif futures_value_1 < 0.5 and futures_value_2 < 0.5 and futures_accury_2 == 0:
+elif futures_value_1 < 0.2 and futures_value_2 < 0.2 and futures_accury_2 == 0:
     pingjia = 'kongtou_ing_kill'
-
 elif futures_value_1 > 0.5:
     pingjia = 'duotou_main'
 elif futures_value_1 < 0.5:
     pingjia = 'kongtou_main'
 else:
-    pingjia = 'unknow_reason'
+    pingjia = 'unknow_reason' 
 
 judge_res = pd.DataFrame({'date':date_now,'pingjia':pingjia},index=[0])
 judge_res.to_csv('res_btc_lianghua.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
